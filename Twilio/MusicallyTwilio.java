67b2c5a280a7ea45e5e0945c5e5d3610a081c149
Abhishek <76660005+abhishek213-alb@users.noreply.github.com>
2022-02-20 16:22:16 +0530

    Updated Twilio Integration

--- Twilio/MusicallyTwilio.java
+++ Twilio/MusicallyTwilio.java
@@ -6,3 +6,18 @@ import com.twilio.type.PhoneNumber;
 import java.net.URI; 
 import java.math.BigDecimal; 
  
+public class Example { 
+    public static final String ACCOUNT_SID = "Enter SID"; 
+    public static final String AUTH_TOKEN = "[Enter Token]"; 
+ 
+    public static void main(String[] args) { 
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN); 
+        Message message = Message.creator( 
+                new com.twilio.type.PhoneNumber("whatsapp: (number)"), 
+                new com.twilio.type.PhoneNumber("whatsapp:Number2"),  
+                "  Welcome to MusicallyWin.us Coffee? or better, start your day with some musical aptitude to get your brain working!")      
+            .create(); 
+ 
+        System.out.println(message.getSid()); 
+    } 
+}
